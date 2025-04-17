46f86d89976e92bd533e743de9a1d1d489443096
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2025-04-17 14:38:12 +0100

    new car smell

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className='text-center align-middle text-8xl font-[poppins]'> 
+      Hello World
+     </div>
+       
+    </>
+  )
+}
+
+export default App
